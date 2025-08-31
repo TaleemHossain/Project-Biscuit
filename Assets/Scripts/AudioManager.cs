e93b01b9e1e71398d5e3831fe790a6778bc25b5f
devanthecoder <anju.blore2015@gmail.com>
2025-08-31 15:15:25 +0530

    Put Background music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource SFX;
+    [SerializeField] AudioSource Music;
+    public AudioClip Background;
     public AudioClip BiteCookie;
     public AudioClip ClickButton;
     public AudioClip CollectPowerUp;
     public AudioClip UsePowerUp;
     public AudioClip GameOver;
     public AudioClip CollectCookie;
+    private void Start()
+    {
+        Music.clip = Background;
+        Music.Play();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlaySFX(AudioClip aud)
     {
